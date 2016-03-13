1aefa0ed2174cb999e9179a598664563e250781e
NishikinoMaki <maki37399@gmail.com>
2016-03-14 01:58:07 +0800

    create test code

---
+++ src/main/java/com/nishikinomaki/SimplifyPath.java
@@ -0,0 +1,22 @@
+package com.nishikinomaki;
+
+/**
+ * Created by nishikinomaki on 16/3/14.
+ *
+ * Given an absolute path for a file (Unix-style), simplify it.
+
+    For example,
+    path = "/home/", => "/home"
+    path = "/a/./b/../../c/", => "/c"
+    @see {https://leetcode.com/problems/simplify-path/}
+ */
+public class SimplifyPath {
+
+    public String simplifyPath(String path) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
